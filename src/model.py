fbaccde7ed85ceb38f09ae8175310efbbb86955c
rplain1 <ryan.plain1@gmail.com>
2024-12-13 16:13:51 -0700

    fix total_wickets > 10

--- src/model.py
+++ src/model.py
@@ -1,6 +1,9 @@
 import polars as pl
 from sklearn.linear_model import LinearRegression
+import joblib
+from pathlib import Path
 
+OUT_DIR = Path("models/")
 df = pl.read_csv("report/q3a.csv")
 
 model = LinearRegression()
@@ -9,3 +12,5 @@ X = df["over"].to_numpy().reshape(-1, 1)
 y = df["runs"].to_numpy()
 
 model.fit(X, y)
+
+joblib.dump(model, OUT_DIR / "linear_model.pkl")
